7ba02e1d8264cf6c5835ca7d0d937cfb203b8b80
ghosts-arch <ronan.questier@mailfence.com>
2025-03-04 14:09:21 +0100

    refact populate_facts()

--- src/core/client.py
+++ src/core/client.py
@@ -30,6 +30,7 @@ class Freudy(discord.Client):
 
         super().__init__(intents=discord.Intents.all())
         self.database = Database()
+        self.database.init()
         self.application_commands = load_application_commands()
         self.config = load_config(path=config_path)
         self.cooldowns = CooldownsManager()
--- src/core/database/database.py
+++ src/core/database/database.py
@@ -35,10 +35,12 @@ class Database:
         Base.metadata.create_all(self.engine)
         Base.set_database(self)
         self.Session = sessionmaker(self.engine, expire_on_commit=False)
+
+    def init(self):
         try:
-            self.populate_questions()
             self.populate_facts()
-        except ValueError as error:
+            self.populate_questions()
+        except Exception as error:
             logger.error(error)
 
     def close(self):
@@ -67,6 +69,7 @@ class Database:
         return result
 
     def populate_questions(self):
+        start_time = time.perf_counter()
         questions = load_json_file("data.json")
         with self.session_scope() as session:
             select_questions_statement = select(Question.question)
@@ -84,9 +87,6 @@ class Database:
                 )
                 for key, value in question_data["answers"].items():
                     if len(value["text"]) > 80:
-                        print(
-                            f"error : {value['text']} ({len(value['text'])} characters)"
-                        )
                         raise ValueError(
                             f"Answer text exceeds max length: {value['text']} ({len(value['text'])} characters)"
                         )
@@ -102,7 +102,10 @@ class Database:
                     )
                     question.answers.append(answer)
                 session.add(question)
-                logger.info("%s added to database.", question)
+        end_time = time.perf_counter()
+        logger.info(
+            f"Populated {len(new_questions)} questions in {(end_time - start_time) * 1000:.2f} seconds."
+        )
 
     def get_daily_facts(self):
         with self.session_scope() as session:
@@ -110,17 +113,17 @@ class Database:
             fact = session.scalars(statement=statement).all()
         return set(fact)
 
-    def populate_facts(self):
+    def populate_facts(self) -> None:
         start_time = time.perf_counter()
         facts = load_json_file("facts.json")
-        existing_facts = self.get_daily_facts()
-        created_facts = [
+        existing_facts: set[str] = self.get_daily_facts()
+        created_facts: list[DailyFact] = [
             DailyFact(fact=fact) for fact in facts if fact not in existing_facts
         ]
         with self.session_scope() as session:
-            session.bulk_save_objects(created_facts)
-        for fact in created_facts:
-            logger.info("%s added to database.", fact)
+            session.bulk_insert_mappings(
+                DailyFact, [fact.__dict__ for fact in created_facts]
+            )
         end_time = time.perf_counter()
         logger.info(
             f"Populated {len(created_facts)} facts in {(end_time - start_time) * 1000:.2f} seconds."
