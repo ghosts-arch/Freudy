11da6dc8733bbc72a11dcf7d4f984076d75ceb1c
ghosts-arch <ronan.questier@mailfence.com>
2025-03-07 01:16:58 +0100

    pylint fix logge

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,14 @@
-# coding : utf-8
-# Python 3.10
-# ----------------------------------------------------------------------------
+"""
+Logger Utility Module
+
+This module provides functionality to set up logging configuration from a YAML file.
+It reads the logging configuration from a file named 'logger.yaml' located in the 
+current directory, loads the configuration using the PyYAML library, and applies it 
+using the logging.config.dictConfig method. It then returns the root logger instance.
+
+Functions:
+    setup_logging: Sets up logging configuration and returns a logger instance.
+"""
 
 import logging.config
 import logging
@@ -9,8 +17,20 @@ import yaml
 
 
 def setup_logging() -> logging.Logger:
+    """
+    Sets up logging configuration from a YAML file and returns a logger instance.
+
+    This function reads the logging configuration from a file named 'logger.yaml' 
+    located in the current directory, loads the configuration using the PyYAML 
+    library, and applies it using the logging.config.dictConfig method. It then 
+    returns the root logger instance.
+
+    Returns:
+        logging.Logger: The root logger instance configured according to the 
+        settings in 'logger.yaml'.
+    """
     logger_config_file = pathlib.Path("logger.yaml")
-    with open(logger_config_file, "r") as f:
+    with open(logger_config_file, "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
     logging.config.dictConfig(config=config)
     return logging.getLogger()
