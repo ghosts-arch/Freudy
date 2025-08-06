e3e0e3c7d0af094615cd834938213e522ea3ccf9
ghosts-arch <ronan.questier@mailfence.com>
2025-08-06 08:49:11 +0200

    try fix daily message duplication

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dotenv
+import asyncio
 
 from src.client import Freudy
 from src.utils.logger import setup_logging
@@ -23,6 +24,7 @@ setup_environment()
 
 def main() -> None:
     client = Freudy()
+    asyncio.run(client.init())
     client_token = os.getenv("CLIENT_TOKEN")
     if not client_token:
         logger.error("CLIENT_TOKEN not found in environment variables or .env file")
