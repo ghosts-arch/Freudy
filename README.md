7ad93ea8a6c422c7fa8c548135f860860cf3b31e
ghosts-arch <ronan.questier@mailfence.com>
2025-08-31 03:52:49 +0200

    import v2 of freudy

--- README.md
+++
@@ -1,4 +0,0 @@
-# Freudy 
-
-Un bot discord qui propose un quiz de 98 questions et 100 annecdotes quotdiennes sur le thême de la psychologie. 
-
--- src/utils/get_credentials.py
+++
@@ -1,5 +0,0 @@
-import os
-from typing import Tuple
-
-def get_credentials() -> Tuple[str | None, str | None]:
-    return os.getenv("APPLICATION_ID"), os.getenv("CLIENT_TOKEN")
