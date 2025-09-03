cb057d76da815700a30bc24b7eb0ef5381b52b6b
ghosts-arch <ronan.questier@mailfence.com>
2025-09-03 16:44:09 +0200

    add question endpoint to api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { DailyFact } from "../database/database";
+import { DailyFact, Question } from "../database/database";
 
 export const APP = express();
 
@@ -7,7 +7,12 @@ APP.get("/", (request, result) => {
   result.send({ message: "Pong !" });
 });
 
-APP.get("/daily_fact", async (request, result) => {
+APP.get("/daily_fact", async (request, response) => {
   const dailyFact = await DailyFact.getRandomDailyFact();
-  result.send({ fact: dailyFact });
+  response.send({ fact: dailyFact });
+});
+
+APP.get("/question", async (request, response) => {
+  const question = await Question.getRandomQuestion();
+  response.json({ question: question });
 });
