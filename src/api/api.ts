5037b896eaa3318da1b99c6b1a435d7cec7eac9c
ghosts-arch <ronan.questier@mailfence.com>
2025-09-03 05:57:00 +0200

    emove cors middleware (useless)

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,17 +3,6 @@ import { DailyFact } from "../database/database";
 
 export const APP = express();
 
-const allowedOrigins = ["127.0.0.1", "freudy-website.vercel.app"];
-APP.use((request, result, next) => {
-  const origin = request.headers.origin;
-  console.log(origin);
-  if (origin) {
-    if (!allowedOrigins.includes(origin)) {
-      return result.status(403).json({ message: "forbidden" });
-    }
-  }
-  next();
-});
 APP.get("/", (request, result) => {
   result.send({ message: "Pong !" });
 });
