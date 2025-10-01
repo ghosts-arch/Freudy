01c6b31fec91d008d56c278b5c55d35c7a096fc8
ghosts-arch <ronan.questier@mailfence.com>
2025-10-01 22:53:05 +0200

    delete API

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { Freudy } from "./client";
 import { config } from "dotenv";
 
 import { info } from "./utils/logging";
-import { APP } from "./api/api";
 
 config();
 
@@ -10,9 +9,3 @@ const client = new Freudy();
 (async () => {
   await client.init();
 })();
-
-APP.listen(process.env.API_PORT);
-process.on("SIGINT", async () => {
-  await info(`Bot is disconnected !`);
-  process.exit(0);
-});
