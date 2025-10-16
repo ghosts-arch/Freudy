62e0aad2787635d4c38bf1d0f6c775702ad80e66
ghosts-arch <ronan.questier@mailfence.com>
2025-10-16 04:52:58 +0200

    file service

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,11 +1,11 @@
-import { Readable } from "node:stream";
-import csv from "csv-parser";
 import { SlashCommandBuilder } from "discord.js";
 import { PERMISSIONS_LEVEL } from "../enums/permissionsLevel";
-import { createQuestion } from "../services/questionsService";
-import type { ICommand } from "./commandInterface";
-
-const ACCEPTED_EXTENSIONS = ["csv", "json"];
+import { processFileParsing } from "../services/fileService";
+import {
+	createQuestion,
+	type QuestionData,
+} from "../services/questionsService";
+import type { ICommand } from "../types/commandInterface";
 
 const uploadCommand: ICommand = {
 	permissionLevel: PERMISSIONS_LEVEL.ADMINISTRATOR,
@@ -21,97 +21,17 @@ const uploadCommand: ICommand = {
 	async execute(context) {
 		await context.interaction.deferReply();
 		const attachment = context.interaction.options.getAttachment("file", true);
-		const fileExtension = attachment.name.split(".")[1];
-		if (!fileExtension) return; // TODO: error , unknown extension
-		if (!ACCEPTED_EXTENSIONS.includes(fileExtension)) {
-			context.interaction.editReply(
-				"Extension de fichier inconnue, veuillez recommencer !",
-			);
-			return;
+		let questionsData: QuestionData[] = [];
+		try {
+			questionsData = await processFileParsing(attachment);
+		} catch (error) {
+			context.sendErrorEmbed(`${error}`);
 		}
-		const result = await fetch(attachment.url);
-		switch (fileExtension) {
-			case "csv":
-				await parseFile(result);
-				break;
-			case "json": {
-				console.log(result);
-				const data = await result.json();
-				for (const question of data) {
-					await createQuestion(question);
-				}
-				break;
-			}
-			default:
-				break;
+		for (const question of questionsData) {
+			await createQuestion(question);
 		}
 		context.interaction.editReply("questions rajoutées !");
 	},
 };
 
 export default uploadCommand;
-
-const parseFile = async (response: Response) => {
-	const buffer = await response.arrayBuffer();
-	const text = Buffer.from(buffer).toString("utf-8");
-	const stream = Readable.from(text);
-	const results: {
-		question: string;
-		answers: { text: string; isValidAnswer: boolean }[];
-	}[] = [];
-	await new Promise<void>((resolve, reject) => {
-		stream.pipe(
-			csv({
-				separator: ";",
-				skipLines: 7,
-				headers: [
-					"id",
-					"Question - max 120 characters",
-					"Answer 1 - max 75 characters",
-					"Answer 2 - max 75 characters",
-					"Answer 3 - max 75 characters",
-					"Answer 4 - max 75 characters",
-					"Time limit (sec) – 5, 10, 20, 30, 60, 90, 120, or 240 secs",
-					"Correct answer(s) - choose at least one",
-				],
-			})
-				.on("headers", (headers) => {
-					console.log(headers);
-				})
-				.on("data", (data) => {
-					if (data["Question - max 120 characters"] === "" || data.id === "")
-						return;
-					const question = {
-						question: data["Question - max 120 characters"],
-						answers: [
-							{
-								text: data["Answer 1 - max 75 characters"],
-								isValidAnswer: true,
-							},
-							{
-								text: data["Answer 2 - max 75 characters"],
-								isValidAnswer: false,
-							},
-							{
-								text: data["Answer 3 - max 75 characters"],
-								isValidAnswer: false,
-							},
-							{
-								text: data["Answer 4 - max 75 characters"],
-								isValidAnswer: false,
-							},
-						],
-					};
-					results.push(question);
-				})
-				.on("end", () => {
-					resolve();
-				})
-				.on("error", reject),
-		);
-	});
-	for (const question of results) {
-		await createQuestion(question);
-	}
-	return;
-};
---
+++ src/services/fileService.ts
@@ -0,0 +1,108 @@
+import { Readable } from "node:stream";
+import csv from "csv-parser";
+import type { Attachment } from "discord.js";
+import type { QuestionData } from "./questionsService";
+
+const ACCEPTED_EXTENSIONS = ["csv", "json"];
+
+export const getFileExtension = (fileName: string): string | null => {
+	return fileName.split(".").pop() ?? null;
+};
+
+export const isAcceptedExtension = (fileExtension: string): boolean => {
+	return ACCEPTED_EXTENSIONS.includes(fileExtension);
+};
+
+export const downloadFile = async (fileUrl: string): Promise<Response> => {
+	return await fetch(fileUrl);
+};
+
+export const parseJson = async (
+	response: Response,
+): Promise<QuestionData[]> => {
+	const data = (await response.json()) as QuestionData[];
+	return data;
+};
+
+export const parseCSV = async (response: Response) => {
+	const buffer = await response.arrayBuffer();
+	const text = Buffer.from(buffer).toString("utf-8");
+	const stream = Readable.from(text);
+	const results: {
+		question: string;
+		answers: { text: string; isValidAnswer: boolean }[];
+	}[] = [];
+	await new Promise<void>((resolve, reject) => {
+		stream.pipe(
+			csv({
+				separator: ";",
+				skipLines: 7,
+				headers: [
+					"id",
+					"Question - max 120 characters",
+					"Answer 1 - max 75 characters",
+					"Answer 2 - max 75 characters",
+					"Answer 3 - max 75 characters",
+					"Answer 4 - max 75 characters",
+					"Time limit (sec) – 5, 10, 20, 30, 60, 90, 120, or 240 secs",
+					"Correct answer(s) - choose at least one",
+				],
+			})
+				.on("headers", (headers) => {
+					console.log(headers);
+				})
+				.on("data", (data) => {
+					if (data["Question - max 120 characters"] === "" || data.id === "")
+						return;
+					const question = {
+						question: data["Question - max 120 characters"],
+						answers: [
+							{
+								text: data["Answer 1 - max 75 characters"],
+								isValidAnswer: true,
+							},
+							{
+								text: data["Answer 2 - max 75 characters"],
+								isValidAnswer: false,
+							},
+							{
+								text: data["Answer 3 - max 75 characters"],
+								isValidAnswer: false,
+							},
+							{
+								text: data["Answer 4 - max 75 characters"],
+								isValidAnswer: false,
+							},
+						],
+					};
+					results.push(question);
+				})
+				.on("end", () => {
+					resolve();
+				})
+				.on("error", reject),
+		);
+	});
+	return results;
+};
+
+export const processFileParsing = async (
+	attachment: Attachment,
+): Promise<QuestionData[]> => {
+	const extension = getFileExtension(attachment.name);
+	if (!extension)
+		throw new Error("Impossible de déterminer l'extension de ce fichier");
+	if (!isAcceptedExtension(extension))
+		throw new Error("Cette extension de fichier n'est pas prise en charge");
+	const response = await downloadFile(attachment.url);
+	let questions: QuestionData[] = [];
+	switch (extension) {
+		case "json":
+			questions = await parseJson(response);
+			break;
+		case "csv":
+			questions = await parseCSV(response);
+			break;
+	}
+	return questions;
+};
