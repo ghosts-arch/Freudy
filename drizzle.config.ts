b8f9dbe4a60cfc7a964d49b4d786db3945eed89f
ghosts-arch <ronan.questier@mailfence.com>
2025-10-21 05:41:16 +0200

    fix schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "drizzle-kit"
 
 export default defineConfig({
     out : "./drizzle",
-    schema : "./src/database/schema.ts",
+    schema : "./src/core/database/schema.ts",
     dialect : "sqlite",
     dbCredentials : {
         url : process.env.DB_FILE_NAME
